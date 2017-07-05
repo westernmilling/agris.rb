9e07459dc1c85d382b51d4aec4ad6b24d85b1f2e
John Gray <wopr42@gmail.com>
2017-07-05 11:08:45 -0500

    Allow user agent configuration ID:2733 (#2)
    https://westernmilling.tpondemand.com/entity/2733
    
    Changes the requester to include a configurable string and the current
    `agris.rb` version.

--- lib/agris/savon_request.rb
+++ lib/agris/savon_request.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'agris/user_agent'
 require 'savon'
 
 module Agris
---
+++ lib/agris/user_agent.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'agris/version'
+
+module Agris
+  USER_AGENT = Agris.user_agent.to_s + " (Agris.rb #{VERSION})".freeze
+end
