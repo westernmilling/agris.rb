f1e45b9b262a864e77c42742799d52551e8825a6
Samantha Torres <storres@westernmilling.com>
2018-07-25 10:46:11 -0700

    refactor model

--- lib/agris/xml_model.rb
+++ lib/agris/xml_model.rb
@@ -41,26 +41,11 @@ module Agris
 
     module ClassMethods
       def from_json_hash(hash)
-        klass = Object.const_get(name)
-        attribute_map =
-          klass::ATTRIBUTE_NAMES
-          .each_with_object({}) do |name, new_hash|
-            new_hash[name.delete('_').to_s] = name
-          end
-
-        translated_hash = hash.each_with_object({}) do |(key, value), new_hash|
-          attribute_name = attribute_map[key]
-
-          new_hash[attribute_name] = value if attribute_name
-          new_hash
-        end
-
-        klass.new(translated_hash)
+        Object.const_get(name).new(hash)
       end
 
       def from_xml_hash(hash)
         klass = Object.const_get(name)
-
         attribute_map =
           klass::ATTRIBUTE_NAMES
           .each_with_object({}) do |name, new_hash|
